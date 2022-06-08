af806a375cbea6099e889eb6fbb49deb6ad4af7c
aissa-laribi <aissalaribi@yahoo.fr>
2022-06-08 20:03:31 +0100

    Changing the website to be scrapped

--- lambda_function.py
+++ lambda_function.py
@@ -1,29 +1,21 @@
-import json
 import urllib.request
-from urllib.request import urlopen
 from bs4 import BeautifulSoup as soup
 import time
 
 def lambda_handler(event, context):
     urls = [
-    'https://www.yell.com/ucs/UcsSearchAction.do?scrambleSeed=631915792&keywords=fitness&location=london&pageNum=1',
-    'https://www.yell.com/ucs/UcsSearchAction.do?scrambleSeed=631915792&keywords=fitness&location=london&pageNum=2',
-    'https://www.yell.com/ucs/UcsSearchAction.do?scrambleSeed=631915792&keywords=fitness&location=london&pageNum=3',
-    'https://www.yell.com/ucs/UcsSearchAction.do?scrambleSeed=631915792&keywords=fitness&location=london&pageNum=4',
-    'https://www.yell.com/ucs/UcsSearchAction.do?scrambleSeed=631915792&keywords=fitness&location=london&pageNum=5',
-    'https://www.yell.com/ucs/UcsSearchAction.do?scrambleSeed=631915792&keywords=fitness&location=london&pageNum=6',
-    'https://www.yell.com/ucs/UcsSearchAction.do?scrambleSeed=631915792&keywords=fitness&location=london&pageNum=7',
-    'https://www.yell.com/ucs/UcsSearchAction.do?scrambleSeed=631915792&keywords=fitness&location=london&pageNum=8',
-    'https://www.yell.com/ucs/UcsSearchAction.do?scrambleSeed=631915792&keywords=fitness&location=london&pageNum=9',
-    'https://www.yell.com/ucs/UcsSearchAction.do?scrambleSeed=631915792&keywords=fitness&location=london&pageNum=10',
-
+        'https://www.yellowpages.com/search?search_terms=fitness&geo_location_terms=New%20York%2C%20NY&page=1',
+        'https://www.yellowpages.com/search?search_terms=fitness&geo_location_terms=New%20York%2C%20NY&page=2',
+        'https://www.yellowpages.com/search?search_terms=fitness&geo_location_terms=New%20York%2C%20NY&page=3',
+        'https://www.yellowpages.com/search?search_terms=fitness&geo_location_terms=New%20York%2C%20NY&page=4',
+        
     ]
     for url in urls:
         headers = { 'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)' }
         request = urllib.request.Request(url, headers=headers)
         response = urllib.request.urlopen(request)
         page_html = response.read()
-        time.sleep(10)
+        time.sleep(5)
         page_soup = soup(page_html, "html.parser")
-        for link in page_soup.find_all('a', class_= "btn btn-yellow businessCapsule--ctaItem",):
+        for link in page_soup.find_all('a', class_= "track-visit-website",):
             print(link.get('href', "a"))
